b9f8d6cee8e05f75eb89d1406196f693da5de829
gzp79 <->
2024-09-28 23:43:12 +0200

    fix some typo

--- src/routes/design/atoms/grids/+page.svelte
+++ src/routes/design/atoms/grids/+page.svelte
@@ -15,8 +15,8 @@
 <div class="w-full items-center">
     <div class="sticky top-0 flex justify-center bg-base-200 p-1">
         <div class="inline-block">
-            Sizes:
-            <span>XS</span>
+            Size:
+            <span class="inline-block">XS</span>
             <span class="hidden md:inline-block">MD</span>
             <span class="hidden lg:inline-block">LG</span>
             <span class="hidden xl:inline-block">XL</span>
@@ -39,14 +39,14 @@
             <GridItem size={{ xs: 6, md: 8, lg: 4 }}>{@render item('xs=6 md=8 lg=4')}</GridItem>
             <GridItem size={{ xs: 6, md: 4, lg: 1 }}>{@render item('xs=6 md=4 lg=1')}</GridItem>
             <GridItem size={{ xs: 6, md: 4, lg: 1 }}>{@render item('xs=6 md=4 lg=1')}</GridItem>
-            <GridItem size={{ xs: 6, md: 8, lg: 2 }}>{@render item('xs=6 md=8 lg=3')}</GridItem>
+            <GridItem size={{ xs: 6, md: 8, lg: 2 }}>{@render item('xs=6 md=8 lg=2')}</GridItem>
         </Grid>
     </div>
 
-    <div class="inline-block">Responsive columns xs=1 md=2 lg=3</div>
+    <div class="inline-block">Responsive columns xs=3 md=4 lg=6</div>
     <div class="m-3">
-        <Grid columns={{ xs: 1, md: 2, lg: 3 }}>
-            <GridItem size={1}>{@render item('s=1')}</GridItem>
+        <Grid columns={{ xs: 3, md: 4, lg: 6 }}>
+            <GridItem size={2}>{@render item('s=2')}</GridItem>
             <GridItem size={2}>{@render item('s=2')}</GridItem>
             <GridItem size={3}>{@render item('s=3')}</GridItem>
             <GridItem size={1}>{@render item('s=1')}</GridItem>
@@ -117,7 +117,7 @@
         </Grid>
     </div>
 
-    <div class="inline-block">Spacing xs=(1,4) md=(2,4) lg=(4,8)</div>
+    <div class="inline-block">Spacing xs=(1,4) md=(2,4) lg=(4,4)</div>
     <div class="m-3">
         <Grid spacing={{ col: { xs: 1, md: 2, lg: 4 }, row: 4 }} columns={2}>
             <GridItem>{@render item('item')}</GridItem>
