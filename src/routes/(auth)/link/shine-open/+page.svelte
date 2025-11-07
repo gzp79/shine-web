84f4dfc6303cf7704d8eee26296471a3fbc72279
gzp79 <->
2025-11-07 13:10:18 +0100

    deep-link page for mobile

--- src/routes/(auth)/link/shine-open/+page.svelte
+++ src/routes/(auth)/link/shine-open/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
     import { identityApi } from '$lib/api/identity-api';
     import AppContent from '$lib/app/AppContent.svelte';
-    import { t } from '$lib/i18n/i18n.svelte';
     import Button from '@atoms/Button.svelte';
     import LoadingCard from '@atoms/LoadingCard.svelte';
-    import Typography from '@atoms/Typography.svelte';
     import ErrorCard from '@components/ErrorCard.svelte';
 
     function redirect(url: string) {
@@ -16,6 +14,7 @@
     {#await identityApi.createSingleAccessToken()}
         <LoadingCard />
     {:then token}
+        <LoadingCard />
         {redirect(`/public/mobile?token=${encodeURIComponent(token)}`)}
     {:catch error}
         <ErrorCard {error}>
